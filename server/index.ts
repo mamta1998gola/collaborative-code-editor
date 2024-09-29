de929e8dbcf6a4ac8cc183c9fb3944c7bd45be3f
mamta1998gola <mamta1998gola@gmail.com>
2024-09-29 18:06:09 +0530

    added app url

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173", // Vite default port
+        origin: "https://collaborative-code-editor-ui.vercel.app/", // Vite default port
         methods: ["GET", "POST"]
     }
 });
